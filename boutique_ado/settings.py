7a39a560627f69a7e055c20be9f792d8bf433c0b
Shay <sleacy@googlemail.com>
2025-05-21 14:14:42 +0100

    Completed homepage header and CSS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = "django-insecure-gzp^d+v_rzn8*u^b$0*l^804!#lq_drtn%yk#8tzfos6b6s18@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -146,6 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
